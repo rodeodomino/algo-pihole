c8464ff964fbc70c06354a31902b59e4c8f3e697
rodeodomino <stuartshapiro@gmail.com>
2020-01-18 17:09:54 -0500

    Fixing tests

--- tests/cloud-init.sh
+++ tests/cloud-init.sh
@@ -11,7 +11,7 @@ export ENDPOINT=10.0.8.100
 export USERS=desktop,user1,user2
 export EXTRA_VARS='install_headers=false tests=true local_service_ip=172.16.0.1'
 export ANSIBLE_EXTRA_ARGS=''
-export REPO_SLUG=${REPOSITORY:-trailofbits/algo}
+export REPO_SLUG=${REPOSITORY:-rodeodomino/algo-pihole}
 export REPO_BRANCH=${BRANCH:-master}
 
 curl -s https://raw.githubusercontent.com/${REPOSITORY:-trailofbits/algo}/${BRANCH:-master}/install.sh | sudo -E bash -x"
